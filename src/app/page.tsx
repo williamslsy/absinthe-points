8259f545eb849f5522b7f0ac3adfcbe98da04838
dev_waliba <donwaleyb@gmail.com>
2024-05-25 10:58:39 +0100

    add toast notifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,13 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import usePointsDistribution from '../hooks/usePointsDistribution';
 import { formSchema } from '@/lib/validations';
+import { DistributePoints } from '@/lib/types';
 
 export default function Home() {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors, isSubmitting },
   } = useForm({
     resolver: zodResolver(formSchema),
@@ -21,20 +23,23 @@ export default function Home() {
       eventName: '',
     },
   });
-
+  const onSubmit = async (data: DistributePoints) => {
+    await handleDistribute(data);
+    reset();
+  };
   const { handleDistribute } = usePointsDistribution();
 
   return (
     <div className="max-w-[900px] mx-auto px-4 py-8 text-black">
       <h1 className="text-black text-3xl font-bold text-center mb-8">Distribute Points</h1>
-      <form onSubmit={handleSubmit(handleDistribute)} className="grid grid-cols-1 gap-4">
-        <Input type="text" placeholder="API Key" {...register('apiKey')} />
+      <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 gap-4">
+        <Input type="search" placeholder="API Key" {...register('apiKey')} />
         {errors.apiKey && <p className="text-red-500">{errors.apiKey.message}</p>}
 
-        <Input type="text" placeholder="Event Name" {...register('eventName')} />
+        <Input type="search" placeholder="Event Name" {...register('eventName')} />
         {errors.eventName && <p className="text-red-500">{errors.eventName.message}</p>}
 
-        <Input type="text" placeholder="Address" {...register('address')} />
+        <Input type="search" placeholder="Address" {...register('address')} />
         {errors.address && <p className="text-red-500">{errors.address.message}</p>}
 
         <Input type="number" placeholder="Points" {...register('points')} />
