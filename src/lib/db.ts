da56a3630dae9112a5bd0cd0600cfd8cb5be1ea7
dev_waliba <donwaleyb@gmail.com>
2024-05-25 13:50:35 +0100

    revert prod check

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,21 +1,2 @@
-import { PrismaClient } from '@prisma/client';
-
-let prisma;
-
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-if (process.env.NODE_ENV === 'production') {
-  // In production always create a new Prisma Client
-  prisma = new PrismaClient();
-} else {
-  // In development or other environments, reuse the instance
-  // Check if it already exists on the global object (in Node.js)
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-
-export { prisma };
+import { Projects, PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient({});
